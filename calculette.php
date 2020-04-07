aff84a606c4747009985be72cd77b188b8d02680
Arthur <boulangerarthur@gmail.com>
2020-04-07 15:30:09 +0200

    création dune calculette et dun fichier de test de travail php

---
+++ calculette.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$a = 7;
+$b = 3;
+$addition = $a + $b;
+echo ($addition);
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+$prenom1 = 'Arthur';
+$prenom2 = 'Quentin';
+echo('Hello '.$prenom1);	
+echo(' et '.$prenom2);
+
+echo('Hello '.$prenom1);	
+echo(' Hello '.$prenom2);
+
