324968e7d6bcf35b2bac386ee934ecaead1f0598
Arthur <boulangerarthur@gmail.com>
2020-04-07 17:08:08 +0200

    correction de la syntaxe du switch

--- calculette.php
+++ calculette.php
@@ -4,15 +4,23 @@ $operator = $argv[2];
 $operant1 = $argv[1];
 $operant2 = $argv[3];
 
+//var_dump($operator);
+//var_dump($argv);
+
 switch ($operator) {
 	case '+':
 		$resultat = $operant1 + $operant2;
+		break;
 	case '-':
 		$resultat = $operant1 - $operant2;
+		break;
 	case '*':
 		$resultat = $operant1 * $operant2;
+		break;
 	case '/':
 		$resultat = $operant1 / $operant2;
+		break;
 }
 
 echo ($resultat);
+
