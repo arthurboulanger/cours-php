2ad125e6ece05f56b2e4c5d79c583d6d86723ba5
Arthur <boulangerarthur@gmail.com>
2020-04-10 11:27:27 +0200

    j'affiche l'heure sur mon catalogue de livre pour mes utilisateurs

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+$time = date("H:i");
+echo "Arthuro ";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ma librairie</title>
+</head>
+<body>
+  <section>
+    <h1>Ma librarie qui va défoncer Amazon</h1>
+    <p>
+      Bienvenue Arthur sur <strong>killamazon.fr</strong></br>
+      Il est <?php echo $time ?> mon petit pépère </br>
+      Ici vous allez pouvoir retrouver tous vos livres préférés à des prix défiant toute concurrence.
+    </p>
+  </section>
+  <section>
+    <div>
+      <ul>
+          <li>
+            <strong>Titre: </strong>Le pendu de Conakry</br>
+          </li>
+          <li>
+            <strong>Auteur: </strong>Jean-Christophe Rufin</br>
+          </li>
+          <li>
+            <strong>ISBN: </strong>2072713277</br>
+          </li>
+          <li>
+            <strong>Prix: </strong>15€</br>
+          </li>
+        </ul>
+    </div>
+    <div>
+      <ul>
+          <li>
+            <strong>Titre: </strong>Le Capital (Tome 1-Livre I) </br>
+          </li>
+          <li>
+            <strong>Auteur: </strong>Karl Marx</br>
+          </li>
+          <li>
+            <strong>ISBN: </strong>2070355748</br>
+          </li>
+          <li>
+            <strong>Prix: </strong>10€</br>
+          </li>
+        </ul>
+    </div>
+    <div>
+      <ul>
+          <li>
+            <strong>Titre: </strong>Living Documentation</br>
+          </li>
+          <li>
+            <strong>Auteur: </strong>Cyrille Martraire</br>
+          </li>
+          <li>
+            <strong>ISBN: </strong>0134689321</br>
+          </li>
+          <li>
+            <strong>Prix: </strong>40€</br>
+          </li>
+        </ul>
+    </div>
+  </section>
+</body>
+</html>
